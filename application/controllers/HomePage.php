2fd3f041f6dcde1e71fd4efe0c0e0d7ca6f722f2
Hamza Sattar <hamzaeffect@gmail.com>
2017-05-03 22:57:00 +0500

    LMS - 03/05/17 Dynamic Home page rendering for different account types

--- application/controllers/HomePage.php
+++ application/controllers/HomePage.php
@@ -12,6 +12,28 @@ class HomePage extends CI_Controller
 
 	public function index()
 	{
-		$this->load->view('home');			
+		if(!empty($this->session->userdata['session_array']['type']))
+		{
+			if($this->session->userdata['session_array']['type'] == 'admin')
+			{
+				$this->load->view('home_admin');
+			}
+			else if ($this->session->userdata['session_array']['type'] == 'student')
+			{
+				$this->load->view('home_student');	
+			}
+			else if ($this->session->userdata['session_array']['type'] == 'teacher')
+			{
+				$this->load->view('home_teacher');	
+			}
+			else if ($this->session->userdata['session_array']['type'] == 'parent')
+			{
+				$this->load->view('home_parent');
+			}	
+		}
+		else
+		{
+			redirect('/Authentication', 'refresh');	
+		}			
 	}
 }
